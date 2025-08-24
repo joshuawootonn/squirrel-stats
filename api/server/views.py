ef2b7dbef8e6d370783e894df9c589d47e9afeab
Joshua Wootonn <joshuawootonn@gmail.com>
2025-08-23 23:29:33 -0500

    chore: clean up formating error in view
    W605 invalid escape sequence '\>'

--- api/server/views.py
+++ api/server/views.py
@@ -2,16 +2,18 @@ from django.http import HttpResponse
 
 
 def index(request):
-    squirrel_art = """
+    squirrel_art = r"""
     <pre style="font-family: monospace; line-height: 1.2;">
   __
- ' )`\\
+ ' )`\
   /' |_./<
- |  /'-.\\_|
- `\\|/~\>>
-   `\\_<_
+ |  /'-.\_|
+ `\|/~\>>
+   `\_<_
 
 Collecting nuts per usual... (Squirrel ASCII art by David Lifson)
+
+
     </pre>
     """
     return HttpResponse(squirrel_art)
